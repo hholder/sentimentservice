87e4b8ac322ef8acc2acaf9b22c9e8701acecf1e
hholder <hasani.holder@gmail.com>
2014-10-14 19:07:37 -0400

    Added NER but still tweaking the functionality

---
+++ SentimentService.ContentAnalysis/NamedEntity.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentService.ContentAnalysis
+{
+    public class NamedEntity : TableEntity
+    {
+        public string EntityName { get; set; }
+        public string EntityType { get; set; }
+        public int Occurences { get; set; }
+        public int TotalSentiment { get; set; }
+
+        public NamedEntity()
+        {
+
+        }
+
+        public NamedEntity(string type, string name, int occurs, int sent)
+        {
+            this.PartitionKey = type;
+            this.RowKey = name;
+            this.EntityName = name;
+            this.EntityType = type;
+            this.Occurences = occurs;
+            this.TotalSentiment = sent;
+        }
+    }
+}
---
+++ SentimentService.ContentRetrieval/WebNewsArticleEntity.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentService.ContentRetrieval
+{
+    public class WebNewsArticleEntity : TableEntity
+    {
+        public string Title { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateRetrieved { get; set; }
+        public string Content { get; set; }
+        public string Source { get; set; }
+
+        public WebNewsArticleEntity()
+        {
+
+        }
+        public WebNewsArticleEntity(string partitionKey, 
+            WebNewsArticle article)
+        {
+            this.PartitionKey = partitionKey;
+            this.RowKey = article.Title;
+            this.Title = article.Title;
+            this.DateCreated = article.DateCreated;
+            this.DateRetrieved = article.DateRetrieved;
+            this.Content = article.Content;
+            this.Source = article.Source;
+        }
+    }
+}
